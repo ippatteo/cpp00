ff5b589a4437a1bb7f495e325ca95419504230ae
Matteo Camilli <mcamilli@e4r14p2.42roma.it>
2024-10-03 12:21:21 +0200

    oki

--- ex01/contact.cpp
+++ ex01/contact.cpp
@@ -7,3 +7,44 @@ Contact::Contact()
 Contact::~Contact()
 {
 }
+
+void setFirst(std::string str)
+{
+	Contact.F = First_Name;
+}
+void setLast(std::string str)
+{
+
+}
+void setNick(std::string str)
+{
+
+}
+void setPhone(std::string str)
+{
+
+}
+void setDark(std::string str)
+{
+
+}
+std::string getFirst()
+{
+
+}
+std::string getLast()
+{
+
+}
+std::string getNick()
+{
+
+}
+std::string getPhone()
+{
+
+}
+std::string getDark()
+{
+
+}
--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -8,12 +8,23 @@
 
 class Contact
 {
-    public:
+	private:
     std::string First_Name;
     std::string Last_Name;
     std::string Nickname;
     std::string Phone_Number;
     std::string Darkest_Secret;
+	public:
+	void setFirst(std::string str);
+	void setLast(std::string str);
+	void setNick(std::string str);
+	void setPhone(std::string str);
+	void setDark(std::string str);
+	std::string getFirst();
+	std::string getLast();
+	std::string getNick();
+	std::string getPhone();
+	std::string getDark();
 	Contact();
 	~Contact();
 };
